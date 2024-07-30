20e4ef514d9ba0944daa6507eb9db227f0990b84
Jamie Letts <jamieletts2210@gmail.com>
2024-07-30 11:49:25 +0100

    test TDD on ApplicationController

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -8,11 +8,21 @@ import javax.inject.{Inject, Singleton}
 @Singleton
 class ApplicationController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
-  def index() = TODO
-  def create() = TODO
-  def read(id: String) = TODO
-  def update(id: String) = TODO
-  def delete(id: String) = TODO
+  def index()= Action {
+    Ok("index test!")
+  }
+  def create() = Action {
+    Ok("create test!")
+  }
+  def read(id: String) = Action {
+    Ok("read test!")
+  }
+  def update(id: String) = Action {
+    Ok("update test!")
+  }
+  def delete(id: String) = Action {
+    Ok("update test!")
+  }
 
 }
 
--- test/controllers/ApplicationControllerSpec.scala
+++ test/controllers/ApplicationControllerSpec.scala
@@ -2,6 +2,10 @@ package controllers
 
 import baseSpec.BaseSpecWithApplication
 
+import play.api.test.FakeRequest
+import play.api.http.Status
+import play.api.test.Helpers._
+
 class ApplicationControllerSpec extends BaseSpecWithApplication{
 
   val TestApplicationController = new ApplicationController(
@@ -10,22 +14,50 @@ class ApplicationControllerSpec extends BaseSpecWithApplication{
 
   "ApplicationController .index()" should {
 
+    val result = TestApplicationController.index()(FakeRequest())
+
+    "return TODO" in {
+      status(result) shouldBe Status.OK
+    }
+
   }
 
   "ApplicationController .create()" should {
 
+    val result = TestApplicationController.create()(FakeRequest())
+
+    "return TODO" in {
+      status(result) shouldBe Status.OK
+    }
   }
 
   "ApplicationController .read()" should {
 
+    val result = TestApplicationController.read("String")(FakeRequest())
+
+    "return TODO" in {
+      status(result) shouldBe Status.OK
+    }
   }
 
   "ApplicationController .update()" should {
 
+
+    val result = TestApplicationController.update("String")(FakeRequest())
+
+    "return TODO" in {
+      status(result) shouldBe Status.OK
+    }
   }
 
   "ApplicationController .delete()" should {
 
+
+    val result = TestApplicationController.delete("String")(FakeRequest())
+
+    "return TODO" in {
+      status(result) shouldBe Status.OK
+    }
   }
 
 }
